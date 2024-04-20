f68ec93c3f5316583d277ec459c5dbc44d7e2d29
Hammad raheem <hammad.raheem@visionetsystems.com>
2024-04-20 22:45:21 +0500

    updated

--- terraform/8-node-pools.tf
+++ terraform/8-node-pools.tf
@@ -4,8 +4,8 @@ resource "google_service_account" "kubernetes" {
 }
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_node_pool
-resource "google_container_node_pool" "general" {
-  name       = "general"
+resource "google_container_node_pool" "dev" {
+  name       = "dev"
   cluster    = google_container_cluster.cluster1dev.id
   node_count = 1
 
@@ -13,17 +13,13 @@ resource "google_container_node_pool" "general" {
     auto_repair  = true
     auto_upgrade = true
   }
-  
-  autoscaling {
-    min_node_count = 0
-    max_node_count = 10
-  }
+
   node_config {
     preemptible  = false
     machine_type = "e2-small"
 
     labels = {
-      role = "general"
+      role = "dev"
     }
 
     service_account = google_service_account.kubernetes.email
@@ -33,3 +29,37 @@ resource "google_container_node_pool" "general" {
   }
 }
 
+resource "google_container_node_pool" "prod" {
+  name    = "prod"
+  cluster = google_container_cluster.cluster2prod.id
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 10
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-small"
+
+    labels = {
+      team = "prod"
+    }
+
+    #taint {
+      #key    = "instance_type"
+      #value  = "spot"
+      #effect = "NO_SCHEDULE"
+    }
+
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
